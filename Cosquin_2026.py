d882ad01ca2c49be78a80456fd845f223b3d7ac4
Camilo Arteaga Mora <camiloarteagaa@gmail.com>
2026-02-02 17:20:45 -0500

    Update front-end instructions for band selection

--- Cosquin_2026.py
+++ Cosquin_2026.py
@@ -115,7 +115,7 @@ matrix_df = matrix_df.loc[(matrix_df != "").any(axis=1)]
 
 # --- FRONT END ---
 st.subheader(f"Día {dia_sel}")
-st.write("Escribí '*' o 'OK' al lado del nombre para marcar tus bandas. Los horarios respetan los desfasajes exactos.")
+st.write("Marcar con 'OK' al lado de la banda")
 
 st.data_editor(
     matrix_df,
